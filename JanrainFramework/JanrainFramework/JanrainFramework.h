d5e921cbda3b11171023cfcd29297536be22c867
James Wilson <james.wilson@rodale.com>
2015-11-06 10:57:06 -0500

    Removed Sample and Test folders

---
+++ JanrainFramework/JanrainFramework/JanrainFramework.h
@@ -0,0 +1,96 @@
+//
+//  JanrainFramework.h
+//  JanrainFramework
+//
+//  Created by Wilson, James on 11/6/15.
+//  Copyright © 2015 Rodale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JanrainFramework.
+FOUNDATION_EXPORT double JanrainFrameworkVersionNumber;
+
+//! Project version string for JanrainFramework.
+FOUNDATION_EXPORT const unsigned char JanrainFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JanrainFramework/PublicHeader.h>
+
+
+//JRCapture/Classes
+#import "JRCapture.h"
+#import "JRCaptureApidInterface.h"
+#import "JRCaptureConfig.h"
+#import "JRCaptureData.h"
+#import "JRCaptureEnvironment.h"
+#import "JRCaptureError.h"
+#import "JRCaptureFlow.h"
+#import "JRCaptureObject+Internal.h"
+#import "JRCaptureObject.h"
+#import "JRCaptureTypes.h"
+#import "JRCaptureUIRequestBuilder.h"
+#import "JRCaptureUser+Extras.h"
+#import "JREngageWrapper.h"
+#import "JRNSDate+ISO8601_CaptureDateTimeString.h"
+#import "JRTraditionalSigninViewController.h"
+#import "NSMutableDictionary+JRDictionaryUtils.h"
+
+//JRConnectionManager
+#import "JRConnectionManager.h"
+#import "NSDictionary+JRQueryParams.h"
+#import "NSMutableURLRequest+JRRequestUtils.h"
+#import "NSURLRequest+JRQueryParams.h"
+
+//JREngage/Classes
+#import "debug_log.h"
+#import "JRActivityObject.h"
+#import "JRCompatibilityUtils.h"
+#import "JREngage+CustomerInterface.h"
+#import "JREngage.h"
+#import "JREngageError.h"
+#import "JREngagePhonegapPlugin.h"
+#import "JRInfoBar.h"
+#import "JRJsonUtils.h"
+#import "JRNativeAuth.h"
+#import "JRNativeAuthConfig.h"
+#import "JRNativeFacebook.h"
+#import "JRNativeGooglePlus.h"
+#import "JRNativeProvider.h"
+#import "JRNativeTwitter.h"
+#import "JRPreviewLabel.h"
+#import "JRProvidersController.h"
+#import "JRPublishActivityController.h"
+#import "JRSessionData.h"
+#import "JRUserInterfaceMaestro.h"
+#import "JRUserLandingController.h"
+#import "JRWebViewController.h"
+
+//Generated/Classes
+#import "JRAccountsElement.h"
+#import "JRAddressesElement.h"
+#import "JRBodyType.h"
+#import "JRCaptureUser.h"
+#import "JRClientsElement.h"
+#import "JRCurrentLocation.h"
+#import "JRDevicesElement.h"
+#import "JRDevicesTemp.h"
+#import "JREmailsElement.h"
+#import "JRFavoritesElement.h"
+#import "JRImsElement.h"
+#import "JRLocation.h"
+#import "JRName.h"
+#import "JROrganizationsElement.h"
+#import "JRPhoneNumbersElement.h"
+#import "JRPhotosElement.h"
+#import "JRPrimaryAddress.h"
+#import "JRProfile.h"
+#import "JRProfilePhotosElement.h"
+#import "JRProfilesElement.h"
+#import "JRRunnersWorld.h"
+#import "JRRunnersWorldFavoritesTemp.h"
+#import "JRRunnersWorldGoApp.h"
+#import "JRStatusesElement.h"
+#import "JRTemporaryData.h"
+#import "JRTrainingPlanSelection.h"
+#import "JRUrlsElement.h"
+#import "JRUserSettings.h"
